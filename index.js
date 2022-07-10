65282af94e0e041203d2e72df495fc19bfaac479
Illia <iluhaar@gmail.com>
2022-07-10 15:39:52 +0300

    split html and js

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.GLOBE({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+  });
+});
