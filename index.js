d8d89d6bf0d3d092afa455eb5de3755092a53082
iluhaar <iluhaar@gmail.com>
2022-07-14 22:16:29 +0300

    changes

---
+++ index.js
@@ -0,0 +1,24 @@
+const checkbox = document.querySelector(".checkbox");
+const vanta = document.querySelector("#vanta");
+
+window.addEventListener("DOMContentLoaded", () => {
+  VANTA.GLOBE({
+    el: "#vanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+  });
+});
+
+checkbox.addEventListener("click", (e) => {
+  const state = e.target.checked;
+  if (state) {
+    vanta.classList.add("hidden");
+  } else if (!state) {
+    vanta.classList.remove("hidden");
+  }
+});
